e4894adfe3b208542b1621d70dc0cc18589d1266
William Pineda <00225919@uca.edu.sv>
2023-06-07 21:18:40 -0600

    Formato Base De React APP

---
+++ healthy/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+import Login from'./pages/login/login';
+
+function App() {
+  return (
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>
+  );
+}
+
+export default App;
