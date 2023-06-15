6790975c72e50fc864c701b8d3bb6cf6a8d4f40f
William Pineda <00225919@uca.edu.sv>
2023-06-14 18:05:46 -0600

    Add: Header

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,85 @@
+//import './App.css';
+import React from "react";
+import "./main.css";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import Header from "../Header/Header";
+
+const Login = (props) => {
+  return (
+    <>
+      <Header />
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div className="Auth-form-container">
+              <form className="Auth-form">
+                <div className="Auth-form-content">
+                  <h3 className="Auth-form-title">LOGIN</h3>
+                  <div className="form-group mt-3">
+                    <label>Nombre de Usuario: </label>
+                    <input
+                      type="text"
+                      className="form-control mt-1"
+                      placeholder="@Carlos21$"
+                    />
+                  </div>
+                  <div className="form-group mt-3">
+                    <label>Contraseña</label>
+                    <input
+                      type="password"
+                      className="form-control mt-1"
+                      placeholder="Contraseña"
+                    />
+                  </div>
+                  <div className="d-grid gap-2 mt-3">
+                    <button type="submit" className="btn btn-primary">
+                      Comenzar
+                    </button>
+                  </div>
+                  <p className="forgot-password text-right mt-2">
+                    Olvidaste tu Contraseña? <a href="#">Click Aqui</a>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-6">
+            <div className="Auth-form-container">
+              <form className="Auth-form">
+                <div className="Auth-form-content">
+                  <h3 className="Auth-form-title">LOGIN</h3>
+                  <div className="form-group mt-3">
+                    <label>Nombre de Usuario: </label>
+                    <input
+                      type="text"
+                      className="form-control mt-1"
+                      placeholder="@Carlos21$"
+                    />
+                  </div>
+                  <div className="form-group mt-3">
+                    <label>Contraseña</label>
+                    <input
+                      type="password"
+                      className="form-control mt-1"
+                      placeholder="Contraseña"
+                    />
+                  </div>
+                  <div className="d-grid gap-2 mt-3">
+                    <button type="submit" className="btn btn-primary">
+                      Comenzar
+                    </button>
+                  </div>
+                  <p className="forgot-password text-right mt-2">
+                    Olvidaste tu Contraseña? <a href="#">Click Aqui</a>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
